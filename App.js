bf782f6d19f5a6886f23b2901592b7f4ea1da35d
LuizSuliman <luizsulimandev@gmail.com>
2022-10-19 08:30:15 -0300

    Styled Components

--- App.js
+++ App.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View, Platform } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View, Platform, Button } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Sharing from 'expo-sharing';
 import * as ImageManipulator from "expo-image-manipulator";
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+    flex: 1;
+    backgroundColor: #fff;
+    alignItems: center;
+    justifyContent: center;
+`;
+const Instructions = styled.Text`
+    color: #000;
+    font-size: 20px;
+    text-align: center;
+    margin: 15px;
+`;
+const Thumbnail = styled.Image`
+    width: 300px;
+    height: 300px;
+    alignSelf: center;
+`;
+const Logo = styled.Image`
+    width: 305;
+    height: 159;
+    marginBottom: 10;
+`;
+const MainButton = styled.TouchableOpacity`
+    background-color: #c495ff;
+    padding: 20px;
+    borderRadius: 5px;
+    margin-top: 20px;
+`;
+
+const ButtonText = styled.Text`
+    fontSize: 20px;
+    color: #fff;
+`;
 
 export default function App() {
   const [selectedImage, setSelectedImage] = React.useState(null);
@@ -26,63 +61,27 @@ export default function App() {
 
   if (selectedImage !== null) {
     return (
-      <View style={styles.container}>
-        <Image
+      <Container>
+        <Thumbnail
           source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
         />
-        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Share this photo</Text>
-        </TouchableOpacity>
-      </View>
+        <MainButton onPress={openShareDialogAsync}>
+          <ButtonText>Share this photo</ButtonText>
+        </MainButton>
+      </Container>
     );
   }
   
   return (
-    <View style={styles.container}>
-        <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
-      <Text style={styles.instructions}>
+    <Container>
+        <Logo source={{ uri: "https://i.imgur.com/TkIrScD.png" }} />
+      <Instructions>
         To share a photo from your phone with a friend, just press the button below!
-      </Text>
-      <TouchableOpacity
-        onPress={openImagePickerAsync}
-        style={styles.button}>
-        <Text style={styles.buttonText}>Pick a photo</Text>
-      </TouchableOpacity>
-    </View>
-  );
+      </Instructions>
+      <MainButton
+        onPress={openImagePickerAsync}>
+        <ButtonText>Pick a photo</ButtonText>
+      </MainButton>
+    </Container>
+   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 305,
-    height: 159,
-    marginBottom: 10,
-  },
-  instructions: {
-    color: '#888',
-    fontSize: 18,
-    marginHorizontal: 15,
-  },
-  button: {
-    backgroundColor: "blue",
-    padding: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
-  },
-  thumbnail: {
-    width: 300,
-    height: 300,
-    resizeMode: "contain"
-  }
-
-});
